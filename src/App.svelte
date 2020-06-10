9991397dfe72cd5fb00500e3739649077d7fd140
dabatistaa <dabatistaa@gmail.com>
2020-06-09 22:14:56 -0500

    Estructura leccion

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Main from './components/Main.svelte';
+	import Leccion from './components/Leccion.svelte';
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<Header />
+<Main>
+	<Leccion />
+</Main>
